978a00ca897bd8083d982f3df35144b8232f9900
Naiztu <A01707404@itesm.mx>
2021-05-06 18:59:48 -0500

    Filtro añadido, laplacian

--- main_root.py
+++ main_root.py
@@ -18,6 +18,9 @@ def filtroBox(file):
 def filtroLaplacian(file):
     laplacian_op(original(file))   
   
+def filtroRep(file):
+    repujado(original(file)) 
+
 def main(): #Metodo principal   
     ventana=tk.Tk() #Contenedor gráfico
     #Caracteriticas de la ventana:
@@ -25,16 +28,16 @@ def main(): #Metodo principal
     
     #Titulo y su formato
     titulo = tk.Label(ventana,font=("Arno Pro Smbd",24),fg="white",text= "Filtros Tumblr", bg="#556677",width=20, height=2)
-    titulo.grid(row=0, column=1, columnspan=2)
+    titulo.grid(row=0, column=1, columnspan=3)
     
     l1 = tk.Label(ventana)
-    l1.grid(row=1, column=1, columnspan=2)
+    l1.grid(row=1, column=1, columnspan=3)
     
     T = tk.Entry(ventana, font=("Arno Pro Smbd",20))
-    T.grid(row=2,column=1, columnspan=2)
+    T.grid(row=2,column=1, columnspan=3)
 
     l2 = tk.Label(ventana)
-    l2.grid(row=3, column=1, columnspan=2)
+    l2.grid(row=3, column=1, columnspan=3)
 
     boton1 = tk.Button(ventana,font=("Arial",14), text="Box blur", command = lambda: filtroBox(T.get()))
     boton1.grid(row=4,column=1,columnspan=1)
@@ -42,8 +45,11 @@ def main(): #Metodo principal
     boton2 = tk.Button(ventana,font=("Arial",14), text="Laplacian", command = lambda: filtroLaplacian(T.get()))
     boton2.grid(row=4,column=2,columnspan=1)
 
+    boton3 = tk.Button(ventana,font=("Arial",14), text="Repujado", command = lambda: filtroRep(T.get()))
+    boton3.grid(row=4,column=3,columnspan=1)
+
     l3 = tk.Label(ventana)
-    l3.grid(row=5, column=1,columnspan=2)
+    l3.grid(row=5, column=1,columnspan=3)
 
     #loop para mantener la ventana siempre visible
     ventana.mainloop()
