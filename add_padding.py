39b8e622ceb4b1fb6ab9e3d0ec8f59d26eb1e301
EmilianoV <emiliano.vasquez@aimexico.org>
2021-05-06 12:12:50 -0800

    Agrega función de filtro con operador laplaciano

--- add_padding.py
+++ add_padding.py
@@ -4,14 +4,15 @@ Función para agregar padding a una matriz
 
 Se calcula una nueva matriz con  cierto grosor de 0s alrededor a partir de las dimensiones del kernel
 """
-def add_padding(matriz, kernel):
-    fil_m, col_m = matriz.shape
-    fil_k, col_k = kernel.shape
-    #Calculamos la cantidad de padding a partir de las dimensiones del kernel
-    pad_height = int((fil_k - 1) / 2)
-    pad_width = int((col_k - 1) / 2)
-    #Creamos matriz con nuevas dimensiones (agregamos padding en los 4 lados)
-    matriz_res = np.zeros(((fil_m + pad_height*2), (col_m + pad_width*2)))
-    matriz_res[pad_height:fil_m + pad_height, pad_width:col_m + pad_width] = matriz
 
-    return(matriz_res)
+def add_padding(matriz, kernel):
+	fil_m, col_m = matriz.shape
+	fil_k, col_k = kernel.shape
+    #Calculamos la cantidad de padding a partir de las dimensiones del kernel
+	pad_height = int((fil_k - 1) / 2)
+	pad_width = int((col_k - 1) / 2)
+    #Creamos matriz con nuevas dimensiones (agregamos padding en los 4 lados)
+	matriz_res = np.zeros(((fil_m + pad_height*2), (col_m + pad_width*2)))
+	matriz_res[pad_height:fil_m + pad_height, pad_width:col_m + pad_width] = matriz
+
+	return(matriz_res)
--- filtros.py
+++ filtros.py
@@ -22,5 +22,24 @@ def box_blur(imagen):
 	plt.imshow(imagen_conv, cmap='gray')
 	plt.title("Imagen con filtro aplicado")
 	plt.show()
+
+def laplacian_op(imagen):
+	"""
+	Función para aplicar el operador laplaciano a una imagen y desplegarla
+
+	Se utiliza la función de convolución para aplicar el filtro y la librería matplotlib para mostrar la imagen
+	"""
+	kernel = np.array([[-1, -1, -1],
+					  [-1, 8, -1],
+					  [-1, -1, -1]])
+		
+	if len(imagen.shape) == 3: #Se cambia la imagen a escala de grises si es necesario
+		imagen = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+
+	imagen_conv = convolution(imagen, kernel)
+
+	plt.imshow(imagen_conv, cmap='gray')
+	plt.title("Imagen con filtro aplicado")
+	plt.show()
 	
 
